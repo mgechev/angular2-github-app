deacd430b4ef39500b0093e7c3b7460b64570dad
mgechev <mgechev@gmail.com>
2015-06-18 19:32:31 +0300

    Add ng-if

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -1,11 +1,11 @@
-import {Component, View, NgFor} from 'angular2/angular2';
+import {Component, View, NgFor, NgIf} from 'angular2/angular2';
 
 @Component({
   selector: 'users'
 })
 @View({
   templateUrl: './components/home/home.html?v=<%= VERSION %>',
-  directives: [NgFor]
+  directives: [NgFor, NgIf]
 })
 export class Home {
   users:string[];
